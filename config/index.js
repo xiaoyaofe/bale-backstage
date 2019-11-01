a7da15c3417d039ddafc460f2168ca17580a163b
tao.wang <tao.wang@xyroyale.com>
2019-11-01 18:14:00 +0800

    修改访问IP

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '172.16.10.111', // can be overwritten by process.env.HOST
+    host: '172.16.10.106', // can be overwritten by process.env.HOST
     port: 9688, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
